40cb54ae5ed20847ba10006daa7c827a996db219
JayNlh <JayNlh@git.com>
2022-01-05 14:52:10 +0800

    Update TwoSum

--- src/com/nlhui/study/Leetcode/arrays/TwoSum.java
+++ src/com/nlhui/study/Leetcode/arrays/TwoSum.java
@@ -16,7 +16,7 @@ public class TwoSum {
             //先算出要找的匹配的数是多少
             int anotherNum=target-nums[i];
             //若该数再map中，且下标不等于自己本身，则满足条件返回他们两个的下标
-            if (map.containsKey(anotherNum)&&i!=map.get(nums[i])){
+            if (map.containsKey(anotherNum)&&i!=map.get(anotherNum)){
                 //注意输出顺序，因为得先有anotherNum在里面，才能满足条件，因为anotherNum的索引在前
                 return new int[]{map.get(anotherNum),i};
             }
